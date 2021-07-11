eb3bbd694f20725775d2e0f40a0828978ee666d1
Aryan <aryanknight12@gmail.com>
2021-07-11 12:29:18 +0530

    first commit

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -7,9 +7,13 @@ export default function Post() {
             <div className="postcontainer">
                 <img src="https://source.unsplash.com/random" alt="" className="imagecontainer" />
                 <div className="textcontainer">
-                    <div style={{minWidth:'50px',maxHeight:'50px',maxWidth:'50px',minHeight:'50px',borderRadius:'50%',overflow:'hidden'}}>
-                        <img src="https://source.unsplash.com/random" />
+                    <div style={{padding:'0.5rem'}} >
+                        <img src="https://source.unsplash.com/random" style={{minWidth:'50px',maxHeight:'50px',maxWidth:'50px',minHeight:'50px',borderRadius:'50%',overflow:'hidden'}} />
                     </div>
+                    <div style={{textAlign:'center'}}>
+                        <h1>10 GitHub repositories to become a better JavaScript Developer</h1>
+                    </div>
+                    <div style={{color:'grey',marginTop:'20px',marginBottom:'20px'}}> #javascript #productivity #beginners #github</div>
                 </div>
             </div>
         </div>
