fa3464c30705f7f4958d6ac797187b3ca13e79dd
Aryan <aryanknight12@gmail.com>
2021-07-11 12:51:17 +0530

    first commit

--- src/Components/SideContent/SideContent.jsx
+++ src/Components/SideContent/SideContent.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import "./SideContent.css"
+import Img1 from'../../Images/img1.png'
+import "./SideContent.css";
 
 export default function SideContent() {
     return (
         <div className="sidecontent">
-            
+            <div>
+                <div className="logo" style={{display:'flex',justifyContent:'center'}}>
+                    <img style={{width:'60%'}}src={Img1}></img>
+                </div>
+            </div>
         </div>
     )
 }
