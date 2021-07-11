db8632e6fc291654deceda201fe96c0384525daa
Aryan <aryanknight12@gmail.com>
2021-07-11 12:40:53 +0530

    first commit

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -10,7 +10,22 @@ export default function Post() {
                     <div style={{padding:'0.5rem'}} >
                         <img src="https://source.unsplash.com/random" style={{minWidth:'50px',maxHeight:'50px',maxWidth:'50px',minHeight:'50px',borderRadius:'50%',overflow:'hidden'}} />
                     </div>
-                    <div>
+                    <div style={{textAlign:'center'}}>
+                    <div className="texts">
+                        <h1 style={{marginLeft:'10px',textAlign:'left'}}>10 GitHub repositories to become a better JavaScript Developer</h1>
+                    </div>
+                    <div style={{color:'grey',marginTop:'20px',marginBottom:'20px'}}>
+                        <div style={{marginLeft:'10px'}}> #javascript #productivity #beginners #github</div></div>
+                        </div>
+                </div>
+            </div>
+            <div className="postcontainer" style={{marginTop:'40px'}}>
+                
+                <div className="textcontainer">
+                    <div style={{padding:'0.5rem'}} >
+                        <img src="https://source.unsplash.com/random" style={{minWidth:'50px',maxHeight:'50px',maxWidth:'50px',minHeight:'50px',borderRadius:'50%',overflow:'hidden'}} />
+                    </div>
+                    <div className="texts">
                         <h1 style={{marginLeft:'10px'}}>10 GitHub repositories to become a better JavaScript Developer</h1>
                     </div>
                     <div style={{color:'grey',marginTop:'20px',marginBottom:'20px'}}>
@@ -18,25 +33,12 @@ export default function Post() {
                 </div>
             </div>
             <div className="postcontainer" style={{marginTop:'40px'}}>
-                <img src="https://source.unsplash.com/random" alt="" className="imagecontainer" />
+               
                 <div className="textcontainer">
                     <div style={{padding:'0.5rem'}} >
                         <img src="https://source.unsplash.com/random" style={{minWidth:'50px',maxHeight:'50px',maxWidth:'50px',minHeight:'50px',borderRadius:'50%',overflow:'hidden'}} />
                     </div>
-                    <div>
-                        <h1 style={{marginLeft:'10px'}}>10 GitHub repositories to become a better JavaScript Developer</h1>
-                    </div>
-                    <div style={{color:'grey',marginTop:'20px',marginBottom:'20px'}}>
-                        <div style={{marginLeft:'10px'}}> #javascript #productivity #beginners #github</div></div>
-                </div>
-            </div>
-            <div className="postcontainer" style={{marginTop:'40px'}}>
-                <img src="https://source.unsplash.com/random" alt="" className="imagecontainer" />
-                <div className="textcontainer">
-                    <div style={{padding:'0.5rem'}} >
-                        <img src="https://source.unsplash.com/random" style={{minWidth:'50px',maxHeight:'50px',maxWidth:'50px',minHeight:'50px',borderRadius:'50%',overflow:'hidden'}} />
-                    </div>
-                    <div>
+                    <div className="texts">
                         <h1 style={{marginLeft:'10px'}}>10 GitHub repositories to become a better JavaScript Developer</h1>
                     </div>
                     <div style={{color:'grey',marginTop:'20px',marginBottom:'20px'}}>
